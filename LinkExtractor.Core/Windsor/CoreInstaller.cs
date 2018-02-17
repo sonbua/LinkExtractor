176979ac1f723ab5da4b538b51809f20b4cd837d
Son Nguyen <dinh.son.nguyen@gmail.com>
2018-02-17 10:39:50 +0700

    Add Windsor installer and register dependencies for LinkExtractor.Core project.

---
+++ LinkExtractor.Core/Windsor/CoreInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using LinkExtractor.Core.Aspect.Validation;
+using LinkExtractor.Core.Aspect.Validation.BuiltIn;
+
+namespace LinkExtractor.Core.Windsor
+{
+    public class CoreInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            // processor
+            container.Register(
+                Component
+                    .For<IRequestProcessor>()
+                    .ImplementedBy<RequestProcessor>()
+                    .LifestyleSingleton()
+            );
+            
+            // validators
+            container.Register(
+                Component
+                    .For(typeof(IValidator<>))
+                    .ImplementedBy(typeof(BuiltInValidator<>))
+                    .LifestyleTransient()
+            );
+
+            // validation rules
+            container.Register(
+                Classes
+                    .FromAssembly(Assembly.GetCallingAssembly())
+                    .BasedOn(typeof(IValidationRule<>))
+                    .WithServiceSelf()
+                    .LifestyleTransient()
+            );
+        }
+    }
+}
