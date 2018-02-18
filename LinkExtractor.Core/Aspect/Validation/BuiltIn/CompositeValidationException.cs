960cef7f939004dfb558968182f432811b4f3fa2
Son Nguyen <dinh.son.nguyen@gmail.com>
2018-02-18 12:11:30 +0700

    Add error message to CompositeValidationException (to improve how exception shown to user).

--- LinkExtractor.Core/Aspect/Validation/BuiltIn/CompositeValidationException.cs
+++ LinkExtractor.Core/Aspect/Validation/BuiltIn/CompositeValidationException.cs
@@ -6,6 +6,7 @@ namespace LinkExtractor.Core.Aspect.Validation.BuiltIn
     public class CompositeValidationException : ValidationException
     {
         public CompositeValidationException(List<ValidationResult> validationResults)
+            : base(validationResults[0], validatingAttribute: null, value: null)
         {
             ValidationResults = validationResults;
         }
