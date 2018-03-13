fe4482b14b51988bbd9655b1d3efa970918e759d
Son Nguyen <son.nd@niteco.se>
2018-03-13 16:59:57 +0700

    Add support for nested collection validation using data annotation attribute.

---
+++ R2.Aspect.Validation.Tests/BuiltIn/ValidateCollectionAttributeTest.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using R2.Aspect.Validation.BuiltIn;
+using Xunit;
+
+namespace R2.Aspect.Validation.Tests.BuiltIn
+{
+    public class ValidateCollectionAttributeTest
+    {
+        [Fact]
+        public void PropertyIsNotOfTypeIEnumerable_ThrowsValidationException()
+        {
+            // arrange
+            var command = new ValidateCollectionThatIsNotOfTypeIEnumerableCommand
+            {
+                NonIEnumerableProp = 0
+            };
+
+            var context = new ValidationContext(command);
+            var results = new List<ValidationResult>();
+
+            // act
+            var isValid = Validator.TryValidateObject(command, context, results, validateAllProperties: true);
+
+            // assert
+            Assert.False(isValid);
+            Assert.Equal(1, results.Count);
+        }
+
+        [Fact]
+        public void CollectionsThatHaveInvalidElements_ReturnsInvalidWithCorrectCount()
+        {
+            // arrange
+            var command = new ValidateNestedObjectsThatImplementIEnumerableCommand
+            {
+                NestedListObject = new List<NestedType> {new NestedType()},
+                NestedIListObject = new List<NestedType> {new NestedType()},
+                NestedICollectionObject = new List<NestedType> {new NestedType()},
+                NestedIEnumerableObject = new List<NestedType> {new NestedType()},
+            };
+
+            var context = new ValidationContext(command);
+            var results = new List<ValidationResult>();
+
+            // act
+            var isValid = Validator.TryValidateObject(command, context, results, validateAllProperties: true);
+
+            // assert
+            Assert.False(isValid);
+            Assert.Equal(4, results.Count);
+        }
+
+        private class ValidateCollectionThatIsNotOfTypeIEnumerableCommand
+        {
+            [ValidateCollection]
+            public int NonIEnumerableProp { get; set; }
+        }
+
+        private class ValidateNestedObjectsThatImplementIEnumerableCommand
+        {
+            [ValidateCollection]
+            public List<NestedType> NestedListObject { get; set; }
+
+            [ValidateCollection]
+            public IList<NestedType> NestedIListObject { get; set; }
+
+            [ValidateCollection]
+            public ICollection<NestedType> NestedICollectionObject { get; set; }
+
+            [ValidateCollection]
+            public IEnumerable<NestedType> NestedIEnumerableObject { get; set; }
+        }
+
+        private class NestedType
+        {
+            [Required]
+            public string RequiredProp { get; set; }
+        }
+    }
+}
---
+++ R2.Aspect.Validation/BuiltIn/ValidateCollectionAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace R2.Aspect.Validation.BuiltIn
+{
+    public class ValidateCollectionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                // If the property is required, it should be validated with RequiredAttribute
+                return ValidationResult.Success;
+            }
+
+            if (!(value is IEnumerable enumerable))
+            {
+                return new ValidationResult($"This property {validationContext.DisplayName} is not enumerable.");
+            }
+
+            var results = new List<ValidationResult>();
+
+            foreach (var item in enumerable)
+            {
+                var context = new ValidationContext(item);
+
+                Validator.TryValidateObject(item, context, results, validateAllProperties: true);
+            }
+
+            return results.Any()
+                ? new CompositeValidationResult(results)
+                : ValidationResult.Success;
+        }
+    }
+}
