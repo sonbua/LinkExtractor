60e45b2cc55ca6b9ee87d68d6a6d8ecb76c12848
Son Nguyen <dinh.son.nguyen@gmail.com>
2018-03-29 03:21:04 +0700

    - All exceptions should be serializable - Add R2Exception as the base exception

---
+++ R2/R2Exception.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace R2
+{
+    [Serializable]
+    public class R2Exception : Exception
+    {
+        public R2Exception()
+        {
+        }
+
+        public R2Exception(string message)
+            : base(message)
+        {
+        }
+
+        public R2Exception(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected R2Exception(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
