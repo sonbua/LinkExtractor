679febd3ec301cc3f23e6da8cc5ad9e93bf9976f
Son Nguyen <dinh.son.nguyen@gmail.com>
2018-08-10 15:25:58 +0700

    Use Ensure.That as guard clause argument validation library for R2.Routing.AspNetCore.

--- R2.Routing.AspNetCore/RouteAttribute.cs
+++ R2.Routing.AspNetCore/RouteAttribute.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using EnsureThat;
 
 namespace R2.Routing.AspNetCore
 {
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class RouteAttribute : Attribute
     {
-        private const string _IS_NULL_OR_EMPTY = "{0} is null or empty.";
-
         private string _prefix = string.Empty;
         private string _template;
 
@@ -26,10 +25,7 @@ namespace R2.Routing.AspNetCore
             get => _prefix;
             set
             {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
+                EnsureArg.IsNotNull(value, nameof(value));
 
                 _prefix = value;
             }
@@ -40,10 +36,7 @@ namespace R2.Routing.AspNetCore
             get => _template;
             set
             {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException(string.Format(_IS_NULL_OR_EMPTY, nameof(value)));
-                }
+                EnsureArg.IsNotNullOrEmpty(value, nameof(value));
 
                 _template = value;
             }
